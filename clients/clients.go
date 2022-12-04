c25d1268a4f9be82f443ca536960009e3bf3d017
mymachine707 <mymaxs441@gmail.com>
2022-12-04 13:27:09 +0500

    gRPC init getWay

---
+++ clients/clients.go
@@ -0,0 +1,37 @@
+package clients
+
+import (
+	"mymachine707/config"
+	"mymachine707/protogen/blogpost"
+
+	"google.golang.org/grpc"
+)
+
+type GrpcClients struct {
+	Author  blogpost.AuthorServiceClient
+	Article blogpost.ArticleServiceClient
+}
+
+func NewGrpcClients(cfg config.Config) (*GrpcClients, error) {
+	connAuthor, err := grpc.Dial(cfg.AuthorServiceGrpcHost+cfg.AuthorServiceGrpcPort, grpc.WithInsecure())
+
+	if err != nil {
+		return nil, err
+	}
+
+	// defer conn.Close() // ulanishdan keyin yoppormasligi uchun hozircha defer keremas
+	author := blogpost.NewAuthorServiceClient(connAuthor)
+
+	connArticle, err := grpc.Dial(cfg.ArticleServiceGrpcHost+cfg.ArticleServiceGrpcPort, grpc.WithInsecure())
+
+	if err != nil {
+		return nil, err
+	}
+	// defer conn.Close() // ulanishdan keyin yoppormasligi uchun hozircha defer keremas
+	article := blogpost.NewArticleServiceClient(connArticle)
+
+	return &GrpcClients{
+		Author:  author,
+		Article: article,
+	}, nil
+}
